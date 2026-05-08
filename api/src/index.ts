4c6ad42066c7a1185a550ebc2d4c359a22f8f475
unknown <bintangkusumam@gmail.com>
2026-05-08 13:01:33 +0700

    >_<

--- api/src/index.ts
+++ api/src/index.ts
@@ -19,7 +19,10 @@ import authRoutes from "./routers/auth.router";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:1234', 'http://localhost:5173', 'https://my-note-be.vercel.app/api']
+}));
 app.use('/api/activities', activityRoutes);
 app.use('/api/balances', balanceRoutes);
 app.use('/api/notes', noteRoutes);
